e0bf32bc526c5c86e19c836a0d36c27a02a561b7
Joseph Casabona <josephcasabona@Josephs-MBP.fios-router.home>
2017-07-05 19:33:41 -0400

    Add 2 functions to exclude JS selection when using page builder.

--- public/js/bbrr-public.js
+++ public/js/bbrr-public.js
@@ -2,8 +2,28 @@
 	'use strict';
 
 	$(document).on('ready', function BBRandomRowSelector() {
-		var bb_random_row = Math.floor(Math.random() * $('.fl-row.bbrr-row').length);
-		$('.fl-row.bbrr-row').hide().eq(bb_random_row).show();
+		if ( ! hasParameter('fl_builder') ) {
+			var bb_random_row = Math.floor(Math.random() * $('.fl-row.bbrr-row').length);
+			$('.fl-row.bbrr-row').hide().eq(bb_random_row).show();
+		}
 	});
 
 })( jQuery );
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function hasParameter(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+				return ( null !== results );
+}
